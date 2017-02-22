36cebc1a9c8614065ec60b53667a8666190372bc
zdz <1696498456@qq.com>
2017-02-22 17:13:27 +0800

    vueCore=>vuecore

--- build/dev-server.js
+++ build/dev-server.js
@@ -12,10 +12,6 @@ var BrowserSync = require('browser-sync-webpack-plugin');
 var HtmlWebpackPlguin = require('html-webpack-plugin');
 
 var mixinWebpackConfig = merge.smart(webpackConfig, {
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: '/dist/'
-    },
     watch: true,
     plugins: [
         new CleanWebpackPlugin(['dist'], {
@@ -32,8 +28,8 @@ var mixinWebpackConfig = merge.smart(webpackConfig, {
             }
         }),
         new webpack.optimize.CommonsChunkPlugin({
-            name: 'vueCore',
-            filename: 'vueCore.js'
+            name: 'vuecore',
+            filename: 'vuecore.js'
         }),
         // new webpack.ProgressPlugin(function (percentage, msg) {
         //   console.log('进度：' + Math.round(percentage * 100) + '% ---> ' + msg)
