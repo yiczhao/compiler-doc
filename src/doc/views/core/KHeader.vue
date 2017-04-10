0fb21e1d43132e8b5b666d0fa3c6ea13865fbf02
zdz <1696498456@qq.com>
2017-04-10 18:11:32 +0800

    添加下载按钮

--- src/doc/views/core/KHeader.vue
+++ src/doc/views/core/KHeader.vue
@@ -4,6 +4,12 @@
       <img class="logo" src="../../assets/logo/login-logo.png"
            width="160" alt="LOGO">
     </div>
+    <a href="./dist/ks.zip" target="_blanck" style="background: #fdfefe;
+    float: right;
+    margin: 8px;
+    font-weight: bold;
+    padding: 8px;
+    color: #36474f;">下载</a>
   </nav>
 </template>
 
