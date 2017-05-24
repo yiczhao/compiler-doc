bfb47f0727f8370c06df9fe2f50db580ae76744a
zdz <1696498456@qq.com>
2017-05-24 14:34:37 +0800

    FED-103 文档中添加版本

---
+++ src/doc/views/form/datepicker/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="ibw page-tabs">
+        <a  class="ib tab" title=""
+            v-bind:class="{'active':active=='date-picker'}" 
+            v-link="{name:'date-picker'}">v0.0.1</a>
+        <a  class="ib tab" title=""
+            v-bind:class="{'active':active=='date-picker/v1.0.0'}" 
+            v-link="{name:'date-picker/v1.0.0'}">v1.0.0</a>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {
+        active:''
+      }
+    },
+    route:{
+        data(transiton){
+            console.log(transiton.to)
+            // transiton.to.title
+            this.active = transiton.to.name
+        }
+    }
+  }
+</script>
