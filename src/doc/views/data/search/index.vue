8dc0d081bd6a1abe64e4f9796e880227706b972e
xinjian <xinjian@kashuo.com>
2017-07-09 21:41:56 +0800

    add version for KsSearch component

---
+++ src/doc/views/data/search/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="ibw page-tabs">
+            <a class="ib tab" title=""
+                v-bind:class="{'active': active == 'search/v1.0.0'}" 
+                v-link="{name: 'search/v1.0.0'}">v1.0.0</a>
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+    data () {
+        return {
+            active: ''
+        }
+    },
+    route: {
+            data (transiton) {
+                this.active = transiton.to.name
+            }
+        }
+    }
+</script>
